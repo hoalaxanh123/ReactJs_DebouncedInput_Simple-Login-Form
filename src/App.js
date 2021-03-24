11e6ff067e6a1eee7fd7d9340fe2e21373f952c8
Vuong Nguyen <nguyenvanvuong972@gmail.com>
2021-03-24 20:03:04 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
-function xin_chao(params) {
-  console.log('object :>> ', object);
-   let a = 1;
-   delete a;
-   let object = {}
-   for (name in object) {
-    doSomething(name);  // Noncompliant
-}
-}
+import { useState } from 'react';
+import DebounceInput from './input';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <div>ASDASDas</div>
-          <div>ASDASDas</div>
-      </header>
-    </div>
-  );
+	const onFinish = (val) => {
+    if(!val.value){
+      return
+    }
+    console.log('val :>> ', val);
+    if (val.name === 'userName') {
+			setUsrName(val.value);
+		} else {
+			setPassword(val.value);
+		}
+    console.log("Updating Status");
+    setTimeout(() => {
+      console.log("Updated");
+    }, 2000);
+	};
+	const [ usrName, setUsrName ] = useState('');
+	const [ password, setPassword ] = useState('');
+  const onSubmit=(e)=>{
+    alert("Login success!");
+
+    e.preventDefault();
+  }
+	return (
+		<div className="container">
+      <div className="content">
+			<h1>Simple login form</h1>
+			<form onSubmit={onSubmit}>
+        <label>Username:</label>
+				<DebounceInput
+					placeholder=''
+					type='text'
+					name='userName'
+					value=''
+					size='60'
+					onFinish={onFinish}
+				/>
+				<br />
+        <label>Password:</label>
+				<DebounceInput
+					placeholder=''
+					type='password'
+					name='passWord'
+					value=''
+					size='60'
+					onFinish={onFinish}
+				/>
+        <br/>
+        <button type='submit'>Login</button>
+			</form>
+			<hr />
+      <h1>Your info</h1>
+			<label>UserName: {usrName}</label>
+      <br/>
+			<label>PassWord: {password}</label>
+      </div>
+		</div>
+	);
 }
 
 export default App;
